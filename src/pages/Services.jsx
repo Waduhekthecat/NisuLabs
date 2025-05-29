39f494a3bda5b01e2e75ad63f87d69a238b8656a
Joshua Darby <josh@Joshuas-MBP.lan>
2025-05-29 13:34:05 -0500

    updated css

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -8,14 +8,6 @@ const Services = () => {
       <div className="services-text">
         <h1>NISU Laboratories Offers</h1>
         <h2>DLC Coating Service For the Following Components: </h2>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-          <li>5</li>
-          <li>6</li>
-        </ul>
       </div>
         <Link to="/Contact" className="inquire-btn" >
                 Request Quote
